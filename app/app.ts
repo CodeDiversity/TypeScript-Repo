a749c0f2d58c8cf96fb84d6fd1182f52b6b60db3
CodeDiversity” <mtrew2015@gmail.com>
2023-06-03 01:32:31 -0400

    making changes

---
+++ app/app.ts
@@ -0,0 +1,31 @@
+function startGame() {
+    let playerName: string | undefined = getInputValue('playername');
+    logPlayer(playerName);
+    postScore(100, playerName);
+  }
+  
+  const startButton = document.getElementById('startGame');
+  if (startButton !== null) {
+    startButton.addEventListener('click', startGame);
+  }
+  
+  function getInputValue(elementId: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>(
+      document.getElementById(elementId)
+    );
+    if (inputElement.value === '') {
+      return undefined;
+    } else {
+      return inputElement.value;
+    }
+  }
+  
+  function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+    let scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+  }
+  
+  function logPlayer(name: string = 'MultiMath Player'): void {
+    console.log(`New game starting for player: ${name}`);
+  }
+  
